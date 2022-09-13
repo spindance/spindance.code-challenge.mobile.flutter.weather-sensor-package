0dd3eab3d6b0d0c9dbeb03beb84c2e1e8a75a9ec
Paige Brinks <paige.brinks@spindance.com>
2022-09-13 13:38:04 -0400

    flutter tests

--- test/flutter_weather_sensor_plugin_test.dart
+++ test/flutter_weather_sensor_plugin_test.dart
@@ -7,13 +7,68 @@ void main() {
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+  });
+
+  test('startSensorReadings', () async {
+    var result = 'failure';
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'startSensorReadings') {
+        result = 'success';
+      }
+    });
+
+    await FlutterWeatherSensorPlugin.startSensorReadings();
+    expect(result, 'success');
+  });
+
+  test('startSensorReadings', () async {
+    var result = 'failure';
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'startSensorReadings') {
+        result = 'success';
+      }
+    });
+
+    await FlutterWeatherSensorPlugin.startSensorReadings();
+    expect(result, 'success');
+  });
+
+  test('stopSensorReadings', () async {
+    var result = 'failure';
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'stopSensorReadings') {
+        result = 'success';
+      }
+    });
+
+    await FlutterWeatherSensorPlugin.stopSensorReadings();
+    expect(result, 'success');
+  });
+
+  test('set readingInterval', () async {
+    var interval = 0;
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'set') {
+        interval = methodCall.arguments['readingInterval'];
+      }
+    });
+
+    await FlutterWeatherSensorPlugin.set(12);
+    expect(interval, 12);
+  });
+
+  test('collectReadings', () async {
+    var result = 'failure';
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'collectReadings') {
+        result = 'success';
+      }
+    });
+
+    var stream = await FlutterWeatherSensorPlugin.collectReadings();
+    expect(result, 'success');
+    expect(stream.isBroadcast, true);
   });
 }
